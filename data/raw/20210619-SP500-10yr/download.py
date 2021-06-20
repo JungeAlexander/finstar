d41b6e97e1a0e006a1c4faaaaa7f622f2fdebde1
Alexander Junge <alexander.junge@gmail.com>
2021-06-20 20:59:27 +0200

    Download max data for each symbol

--- data/raw/20210619-SP500-10yr/download.py
+++ data/raw/20210619-SP500-10yr/download.py
@@ -1,19 +1,30 @@
+import logging
+from pathlib import Path
+from time import sleep
+
 import pandas as pd
 import yfinance as yf
 
+logging.basicConfig(level=logging.INFO)
+
+output_dir = Path("./20210620")
+
 # get S&P500 ticker symbols from Wikipedia
 wiki_df = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]
-symbols = list(wiki_df.loc[:, "Symbol"])
-assert len(symbols) == len(set(symbols))
-
-# download ticker data for last 10 years from YFinance
-tickers = yf.Tickers(symbols)
-ticker_df = tickers.history(period="10y")
-
-# save both Wikipedia and YFinance data
+symbols = sorted(wiki_df.loc[:, "Symbol"])
+assert len(symbols) == len(set(symbols)), "S&P500 contains duplicated symbols"
 wiki_df.to_parquet(
-    "20210619-SP500-companies.snappy.parquet", engine="pyarrow", compression="snappy"
+    output_dir / "SP500-companies.snappy.parquet",
+    engine="pyarrow",
+    compression="snappy",
 )
-ticker_df.to_parquet(
-    "20210619-SP500-10yr.snappy.parquet", engine="pyarrow", compression="snappy"
-)
+
+# download ticker data for max timespan from YFinance
+for s in symbols:
+    logging.info(f"Processing {s}")
+    ticker = yf.Ticker(s)
+    ticker_df = ticker.history(period="max")
+    ticker_df.to_parquet(
+        output_dir / f"{s}.snappy.parquet", engine="pyarrow", compression="snappy"
+    )
+    sleep(1)
